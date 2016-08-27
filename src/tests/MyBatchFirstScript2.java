f2e2b057db7fcbe85b23ee59aa43601a2fc0b05d
admin <admin@192.168.1.117>
2016-08-27 16:06:37 +1000

    First Commit

---
+++ src/tests/MyBatchFirstScript2.java
@@ -0,0 +1,125 @@
+package tests;
+import Functions.*;
+import java.io.FileInputStream;
+import java.util.Properties;
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class MyBatchFirstScript2 extends HotelApp_BusinessFunctions{
+ // private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+ // public Properties Prop;
+  
+  @Before
+  public void setUp() throws Exception {
+	  
+	
+  }
+
+  @Test
+  public void testMyfirstwebdriver() throws Exception {
+	  
+	  Prop = new Properties();
+	  Prop.load(new FileInputStream("./SharedUIMap/SharedUIMap.properties"));
+    driver = new FirefoxDriver();
+    baseUrl = "http://www.adactin.com/HotelApp";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  driver.get(baseUrl);
+	  String strfilepath = "./DataPool/HA_SearchResults.xls";
+	  int iRowCount = HA_GF_XLRowCount (strfilepath, "Location");
+	  for (int i=1; i<iRowCount; i++)
+	  {
+	  String strlocation = HA_GF_readXL(i, "Location", strfilepath);
+	HA_BF_Login("sreelatharavula", "pinkeyes9");
+	 // driver.findElement(By.id(Prop.getProperty("Txt_Login_Username"))).clear();
+	 // driver.findElement(By.id(Prop.getProperty("Txt_Login_Username"))).sendKeys("sreelatharavula");
+	  //driver.findElement(By.id(Prop.getProperty("Txt_Login_Password"))).clear();
+	  //driver.findElement(By.id(Prop.getProperty("Txt_Login_Password"))).sendKeys("pinkeyes9");
+	  //driver.findElement(By.id(Prop.getProperty("Txt_Login_Login"))).click();
+    //driver.findElement(By.id("username")).sendKeys("sreelatharavula");
+    //driver.findElement(By.id("password")).clear();
+    //driver.findElement(By.id("password")).sendKeys("pinkeyes9");
+    //driver.findElement(By.id("login")).click();
+    assertTrue(isElementPresent(By.linkText("Logout")));
+       
+    new Select(driver.findElement(By.id("location"))).selectByVisibleText(strlocation);
+    new Select(driver.findElement(By.id("hotels"))).selectByVisibleText("Hotel Creek");
+    new Select(driver.findElement(By.id("room_type"))).selectByVisibleText("Standard");
+    new Select(driver.findElement(By.id("child_room"))).selectByVisibleText("1 - One");
+    driver.findElement(By.id("Submit")).click();
+    driver.findElement(By.id("radiobutton_0")).click();
+    driver.findElement(By.id("continue")).click();
+    driver.findElement(By.id("first_name")).click();
+    driver.findElement(By.id("first_name")).clear();
+    driver.findElement(By.id("first_name")).sendKeys("vhjv");
+    driver.findElement(By.id("last_name")).clear();
+    driver.findElement(By.id("last_name")).sendKeys("jhbvhjbv");
+    driver.findElement(By.id("address")).clear();
+    driver.findElement(By.id("address")).sendKeys("vhhjbvnm\nvhjvhj\nvhjvhjn");
+    driver.findElement(By.id("cc_num")).clear();
+    driver.findElement(By.id("cc_num")).sendKeys("1245454242412414");
+    new Select(driver.findElement(By.id("cc_type"))).selectByVisibleText("VISA");
+    new Select(driver.findElement(By.id("cc_exp_month"))).selectByVisibleText("December");
+    new Select(driver.findElement(By.id("cc_exp_year"))).selectByVisibleText("2015");
+    driver.findElement(By.id("cc_cvv")).clear();
+    driver.findElement(By.id("cc_cvv")).sendKeys("456");
+    driver.findElement(By.id("book_now")).click();
+    Thread.sleep(8000);
+    driver.findElement(By.linkText("Logout")).click();
+    driver.findElement(By.linkText("Click here to login again")).click();
+   
+	  }
+	  driver.quit();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+   
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
+
